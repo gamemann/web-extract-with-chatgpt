fe35eba0bbe73fa5654f5820605a11dfabce5b52
Christian Deacon <christianmdeacon@gmail.com>
2024-12-19 05:58:45 -0500

    Include web data in stdout output type.

--- src/output/types/stdout.py
+++ src/output/types/stdout.py
@@ -2,5 +2,8 @@ from output import OutputBase
 
 class Output(OutputBase):
     def handle_data(self, url: str, extractor: str, web_data: str, resp: str):
+        print("Web Data:")
+        print(web_data)
+        
         print("Reply from ChatGPT:")
         print(resp)
